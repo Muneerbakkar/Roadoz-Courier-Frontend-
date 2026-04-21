49e02e0aa424eb5a575d156f56a21053a8360c6a
Muneerbakkar <muneerbakkar735@gmail.com>
2026-04-21 14:18:35 +0530

    added franchise end point

--- src/services/endpoints.js
+++ src/services/endpoints.js
@@ -6,4 +6,5 @@ export const ENDPOINTS = {
   // PROFILE_IMAGE: "/profile/image",
   CHANGE_PASSWORD_REQUEST: "/profile/change-password/request",
   CHANGE_PASSWORD_VERIFY: "/profile/change-password/verify",
+  FRANCHISE: "/franchise",
 };
